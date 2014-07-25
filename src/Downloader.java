7193d13fd732273347954d875f6db545ffd84f13
Swair Shah <swairshah@gmail.com>
2014-07-25 18:32:12 -0500

    Dowloader class

--- src/Downloader.java
+++ src/Downloader.java
@@ -1,5 +1,45 @@
-/**
- * Created by swair on 7/25/14.
- */
-public class Downloader {
+import java.io.*;
+import java.net.Socket;
+
+public class Downloader extends Thread {
+
+    private String _filename;
+    private int _server_port;
+    private String _server_ip;
+
+    public Downloader(String filename, String ip, int port) {
+        _filename = filename;
+        _server_ip = ip;
+        _server_port = port;
+    }
+
+    public void copyStream(InputStream input, OutputStream output)
+            throws IOException {
+        byte[] buffer = new byte[1024]; // Adjust if you want
+        int bytesRead;
+        while ((bytesRead = input.read(buffer)) != -1) {
+            output.write(buffer, 0, bytesRead);
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            Socket socket = new Socket(_server_ip,_server_port);
+
+            OutputStream outstream = socket.getOutputStream();
+            OutputStreamWriter writer = new OutputStreamWriter(outstream,"UTF-8");
+            writer.write(_filename);
+            writer.write("\r\n");
+            writer.flush();
+
+            InputStream is = socket.getInputStream();
+            FileOutputStream fos = new FileOutputStream(_filename);
+            copyStream(is,fos);
+
+            fos.close();
+            socket.close();
+        } catch(IOException ex) {}
+    }
 }
+
